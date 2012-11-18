978a2d5d39f11620f04f1cabf7f176d3f3f481f7
Fernando Guillen <fguillen.mail@gmail.com>
2012-11-18 12:35:24 +0100

    Some tests

--- lib/transmission_api.rb
+++ lib/transmission_api.rb
@@ -1,9 +1,14 @@
 require_relative "transmission_api/version"
 require "httparty"
 require "json"
-require "recursive_open_struct"
 
 class TransmissionApi
+  attr_accessor :session_id
+  attr_accessor :url
+  attr_accessor :basic_auth
+  attr_accessor :torrent_fields
+  attr_accessor :debug_mode
+
   TORRENT_FIELDS = [
     "id",
     "name",
@@ -19,56 +24,56 @@ class TransmissionApi
   def initialize(opts)
     @url = opts[:url]
     @torrent_fields = opts[:fields] || TORRENT_FIELDS
-    @auth = { :username => opts[:username], :password => opts[:password] } if opts[:username]
+    @basic_auth = { :username => opts[:username], :password => opts[:password] } if opts[:username]
     @session_id = "NOT-INITIALIZED"
+    @debug_mode = opts[:debug_mode] || false
   end
 
   def all
-    puts "XXX: get_torrents"
+    log "get_torrents"
 
     response =
       post(
         :method => "torrent-get",
         :arguments => {
-          :fields => @torrent_fields
+          :fields => torrent_fields
         }
       )
 
-    RecursiveOpenStruct.new( response["arguments"], :recurse_over_arrays => true ).torrents
+    response["arguments"]["torrents"]
   end
 
   def find(id)
-    puts "XXX: get_torrent: #{id}"
+    log "get_torrent: #{id}"
 
     response =
       post(
         :method => "torrent-get",
         :arguments => {
-          :fields => @torrent_fields,
+          :fields => torrent_fields,
           :ids => [id]
         }
       )
 
-    RecursiveOpenStruct.new( response["arguments"], :recurse_over_arrays => true ).torrents.first
+    response["arguments"]["torrents"].first
   end
 
   def create(filename)
-    puts "XXX: add_torrent: #{filename}"
+    log "add_torrent: #{filename}"
 
     response =
       post(
         :method => "torrent-add",
         :arguments => {
-          :filename => filename,
-          :pause => true
+          :filename => filename
         }
       )
 
-    RecursiveOpenStruct.new( response["arguments"]["torrent-added"] )
+    response["arguments"]["torrent-added"]
   end
 
   def destroy(id)
-    puts "XXX: remove_torrent: #{id}"
+    log "remove_torrent: #{id}"
 
     response =
       post(
@@ -79,37 +84,41 @@ class TransmissionApi
         }
       )
 
-    RecursiveOpenStruct.new( response["arguments"]["torrent-added"] )
+    response
   end
 
   def post(opts)
+    http_post(opts).body
+  end
+
+  def http_post(opts)
     post_options = {
       :body => opts.to_json,
-      :headers => { "x-transmission-session-id" => @session_id }
+      :headers => { "x-transmission-session-id" => session_id }
     }
-    post_options.merge!( :basic_auth => @basic_auth ) if @basic_auth
+    post_options.merge!( :basic_auth => basic_auth ) if basic_auth
 
+    log "url: #{url}"
+    log "post_options: #{post_options}"
 
-    puts "XXX: url: #{@url}"
-    puts "XXX: post_options: #{post_options}"
+    response = HTTParty.post( url, post_options )
 
-    response =
-      HTTParty.post(
-        @url,
-        post_options
-      )
-
-    puts "XXX: response.body: #{response.body}"
-    puts "XXX: response.code: #{response.code}"
-    puts "XXX: response.message: #{response.message}"
-    puts "XXX: response.headers: #{response.headers.inspect}"
+    log "response.body: #{response.body}"
+    log "response.code: #{response.code}"
+    log "response.message: #{response.message}"
+    log "response.headers: #{response.headers.inspect}"
 
     # retry connection if session_id incorrect
     if( response.code == 409 )
+      log "changing session_id"
       @session_id = response.headers["x-transmission-session-id"]
-      response = post(opts)
+      response = http_post(opts)
     end
 
     response
+  end
+
+  def log(message)
+    Kernel.puts "[TransmissionApi #{Time.now.strftime( "%F %T" )}] #{message}" if debug_mode
   end
 end
--- test/transmission_api_test.rb
+++ test/transmission_api_test.rb
@@ -1,16 +1,146 @@
 require_relative "test_helper"
 
 class TransmissionApiTest < Test::Unit::TestCase
-  def test_all
-    transmission_api =
-      TransmissionApi.new(
-        :username => "username",
-        :password => "password",
-        :url      => "http://127.0.0.1:9091/transmission/rpc"
+  def setup
+    @transmission_api = TransmissionApi.new( :url => "http://api.url" )
+  end
+
+  def test_post
+    @transmission_api.stubs(:session_id).returns("SESSION-ID")
+    @transmission_api.stubs(:url).returns("http://api.url")
+
+    opts = { :key1 => "value1", :key2 => "value2" }
+
+    opts_expected = {
+      :body => { :key1 => "value1", :key2 => "value2" }.to_json,
+      :headers => { "x-transmission-session-id" => "SESSION-ID" }
+    }
+
+    response_mock =
+      stub(
+        :code => "",
+        :message => "",
+        :headers => "",
+        :body => "body"
       )
 
-    torrents = transmission_api.all
+    HTTParty.expects(:post).with( "http://api.url", opts_expected ).returns( response_mock )
 
-    puts "XXX: torrents: #{torrents}"
+    assert_equal "body", @transmission_api.post(opts)
   end
+
+  def test_post_with_basic_auth
+    @transmission_api.stubs(:session_id).returns("SESSION-ID")
+    @transmission_api.stubs(:url).returns("http://api.url")
+    @transmission_api.stubs(:basic_auth).returns("user_pass")
+
+    opts = { :key1 => "value1" }
+
+    opts_expected = {
+      :body => { :key1 => "value1" }.to_json,
+      :headers => { "x-transmission-session-id" => "SESSION-ID" },
+      :basic_auth => "user_pass"
+    }
+
+    response_mock =
+      stub(
+        :code => "",
+        :message => "",
+        :headers => "",
+        :body => ""
+      )
+
+    HTTParty.expects(:post).with( "http://api.url", opts_expected ).returns( response_mock )
+
+    @transmission_api.post(opts)
+  end
+
+  def test_post_with_409
+    @transmission_api.stubs(:url).returns("http://api.url")
+    @transmission_api.instance_variable_set(:@session_id, "SESSION-ID")
+
+    opts = { :key1 => "value1" }
+
+    opts_expected_1 = {
+      :body => { :key1 => "value1" }.to_json,
+      :headers => { "x-transmission-session-id" => "SESSION-ID" }
+    }
+
+    opts_expected_2 = {
+      :body => { :key1 => "value1" }.to_json,
+      :headers => { "x-transmission-session-id" => "NEW-SESSION-ID" }
+    }
+
+    response_mock_1 =
+      stub(
+        :code => 409,
+        :message => "",
+        :headers => { "x-transmission-session-id" => "NEW-SESSION-ID" },
+        :body => ""
+      )
+
+    response_mock_2 =
+      stub(
+        :code => 200,
+        :message => "",
+        :headers => "",
+        :body => "body"
+      )
+
+    post_sequence = sequence("post_sequence")
+    HTTParty.expects(:post).with( "http://api.url", opts_expected_1 ).returns( response_mock_1 ).in_sequence( post_sequence )
+    HTTParty.expects(:post).with( "http://api.url", opts_expected_2 ).returns( response_mock_2 ).in_sequence( post_sequence )
+
+    assert_equal "body", @transmission_api.post(opts)
+    assert_equal "NEW-SESSION-ID", @transmission_api.instance_variable_get(:@session_id)
+  end
+
+  def test_all
+    opts_expected = {
+      :method => "torrent-get",
+      :arguments => { :fields => "torrent_fields" }
+    }
+    result = { "arguments" => { "torrents" => "torrents" } }
+
+    @transmission_api.stubs(:torrent_fields).returns("torrent_fields")
+    @transmission_api.expects(:post).with( opts_expected ).returns( result )
+
+    assert_equal( "torrents", @transmission_api.all )
+  end
+
+  def test_find
+    opts_expected = {
+      :method => "torrent-get",
+      :arguments => { :fields => "torrent_fields", :ids => [1] }
+    }
+    result = { "arguments" => { "torrents" => ["torrent1"] } }
+
+    @transmission_api.stubs(:torrent_fields).returns("torrent_fields")
+    @transmission_api.expects(:post).with( opts_expected ).returns( result )
+
+    assert_equal( "torrent1", @transmission_api.find(1) )
+  end
+
+  def test_create
+    opts_expected = {
+      :method => "torrent-add",
+      :arguments => { :filename => "filename" }
+    }
+    result = { "arguments" => { "torrent-added" => "torrent-added" } }
+
+    @transmission_api.expects(:post).with( opts_expected ).returns( result )
+
+    assert_equal( "torrent-added", @transmission_api.create( "filename" ) )
+  end
+
+  def test_destroy
+    opts_expected = {
+      :method => "torrent-remove",
+      :arguments => { :ids => [1], :"delete-local-data" => true }
+    }
+
+    @transmission_api.expects(:post).with( opts_expected )
+    @transmission_api.destroy(1)
+  end
+
 end
